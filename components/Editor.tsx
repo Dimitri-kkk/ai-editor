1b9da578bf22dc70ae3d1be6b52a02a82d49994f
dimitri <dyaryarashvili@gmail.com>
2024-12-14 23:20:32 +0400

    feat: image upload structure

---
+++ components/Editor.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Editor () {
+    return (
+        <div>
+            <h1>editor</h1>
+        </div>
+    )
+}
---
+++ components/upload/upload-image.tsx
@@ -0,0 +1,9 @@
+export default function UploadImage () {
+    
+
+    return (
+        <div>
+            <h1>upload image</h1>
+        </div>
+    )
+}
