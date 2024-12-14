5b9bd5a9e207bb920bba7a39a44a291f6eada782
dimitri <dyaryarashvili@gmail.com>
2024-12-14 23:27:37 +0400

    feat: upload-image structure without state management

--- components/upload/upload-image.tsx
+++ components/upload/upload-image.tsx
@@ -1,5 +1,25 @@
+"use client"
+
+import { useDropzone } from "react-dropzone"
+
 export default function UploadImage () {
-    
+    const {} = useDropzone({
+        maxFiles: 1,
+        accept: {
+            'image/png': ['.png'],
+            'image/jpg': ['.jpg'],
+            'image/webp': ['.webp'],
+            'image/jpeg': ['.jpeg'],
+        },
+        onDrop: async (acceptFiles, fileRejections) => {
+            if(acceptFiles.length) {
+                const formData = new FormData();
+                formData.append('image', acceptFiles[0])
+                const objectUrl = URL.createObjectURL(acceptFiles[0])
+                const res = await uploadImage()
+            }
+        }
+    })
 
     return (
         <div>
