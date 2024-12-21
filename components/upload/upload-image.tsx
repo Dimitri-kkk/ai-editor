d8c2ada8a7c3fa6c460ba7cd284e4fe70510eec5
dimitri <dyaryarashvili@gmail.com>
2024-12-21 20:31:25 +0400

    feat: added upload-image component on server side and client side

--- components/upload/upload-image.tsx
+++ components/upload/upload-image.tsx
@@ -2,29 +2,42 @@
 
 import { uploadImage } from "@/server/upload-image";
 import { useDropzone } from "react-dropzone"
+import { Card, CardContent } from "../ui/card";
+import { cn } from "@/lib/utils";
 
 export default function UploadImage () {
-    const {} = useDropzone({
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
         maxFiles: 1,
         accept: {
-            'image/png': ['.png'],
-            'image/jpg': ['.jpg'],
-            'image/webp': ['.webp'],
-            'image/jpeg': ['.jpeg'],
+            "image/png": [".png"],
+            "image/jpg": [".jpg"],
+            "image/webp": [".webp"],
+            "image/jpeg": [".jpeg"],
         },
         onDrop: async (acceptFiles, fileRejections) => {
             if(acceptFiles.length) {
                 const formData = new FormData();
-                formData.append('image', acceptFiles[0])
+                formData.append("image", acceptFiles[0])
                 const objectUrl = URL.createObjectURL(acceptFiles[0])
                 const res = await uploadImage({ image: formData })
+                console.log(res);
             }
         }
     })
 
     return (
-        <div>
-            <h1>upload image</h1>
-        </div>
+        <Card {...getRootProps()} 
+        className={cn(
+          " hover:cursor-pointer hover:bg-secondary hover:border-primary transition-all  ease-in-out ",
+          `${isDragActive ? "animate-pulse border-primary bg-secondary" : ""}`
+        )} >
+            <CardContent className="flex flex-col h-full items-center justify-center px-2 py-24  text-xs ">
+                <input {...getInputProps()} />
+                <div className="flex items-center flex-col justify-center gap-4">
+                    <p className="text-muted-foreground text-2xl">{isDragActive ? "Drop your image here" : "Start by uploading an image"}</p>
+                    <p className="text-muted-foreground">Supported formats: .jpeg .png .webp .jpg</p>
+                </div>
+            </CardContent>
+        </Card>
     )
 }
