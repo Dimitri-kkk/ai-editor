1d11500203d6d99999b56c27920af71a6fa0e33e
dimitri <dyaryarashvili@gmail.com>
2024-12-26 17:41:14 +0400

    feat: added forms to add new layers/set active layers

--- components/layers/layers.tsx
+++ components/layers/layers.tsx
@@ -4,11 +4,15 @@ import { cn } from "@/lib/utils";
 import { useImageStore } from "@/lib/image-store";
 import { Button } from "../ui/button";
 import { Layers2 } from "lucide-react";
+import LayerImage from "./layer-image";
+import LayerInfo from "./layer-info";
 
 export default function Layers() {
     const layers = useLayerStore((state) => state.layers);
     const activeLayer = useLayerStore((state) => state.activeLayer);
     const generating = useImageStore((state) => state.generating);
+    const addLayer = useLayerStore((state) => state.addLayer);
+    const setActiveLayer = useLayerStore((state) => state.setActiveLayer);
 
     return (
         <Card className="basis-[320px] shrink-0 scrollbar-thin scrollbar-track-secondary overflow-y-scroll 
@@ -27,19 +31,39 @@ export default function Layers() {
             </CardHeader>
             <CardContent className="flex-1 flex flex-col">
                 {layers.map((layer, index) => 
-                  <div className={cn("cursor-pointer ease-in-out hover:bg-secondary border border-transparent", {"animate-pulse" : generating})} key={layer.id}>
+                  <div className={cn("cursor-pointer ease-in-out hover:bg-secondary border border-transparent", 
+                  {"animate-pulse" : generating, 
+                  "border-primary": activeLayer.id === layer.id,})} 
+                  key={layer.id}
+                  onClick={() => {
+                    if(generating) return
+                    setActiveLayer(layer.id)
+                  }}
+                  >
                     <div className="relative p-4 flex items-center">
                         <div className="flex gap-2 items-center h-8 w-full justify-between">
                             {!layer.url ? (
                                 <p className="text-xs font-medium justify-end">New layer</p>
                             ) : null}
+                            <LayerImage layer={layer} />
+                            <LayerInfo layer={layer} layerIndex={index} />
                         </div>
                     </div>
                   </div>
                 )}
             </CardContent>
             <div className="sticky bottom-0 bg-card flex gap-2 shrink-0">
-                <Button className="w-full flex gap-2" variant={"outline"}>
+                <Button onClick={() => {
+                    addLayer({
+                        id: crypto.randomUUID(),
+                        url: "",
+                        height: 0,
+                        width: 0,
+                        publicId: "",
+                        name: "",
+                        format: "",
+                    })
+                }} className="w-full flex gap-2" variant={"outline"}>
                     <span>Create Layer</span>
                     <Layers2 size={18} className="text-secondary-foreground" />
                 </Button>
--- components/upload/upload-image.tsx
+++ components/upload/upload-image.tsx
@@ -62,6 +62,8 @@ export default function UploadImage () {
         }
     })
 
+    if(!activeLayer.url)
+
     return (
         <Card {...getRootProps()} 
         className={cn(
