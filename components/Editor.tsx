df07185472182411e0952396bc92a263bd5ad94c
dimitri <dyaryarashvili@gmail.com>
2024-12-26 16:13:14 +0400

    feat: added activeImage component

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -3,16 +3,18 @@
 import { ModeToggle } from "@/components/theme/mode-toggle"
 import Layers from "./layers/layers"
 import UploadImage from "./upload/upload-image"
+import ActiveImage from "./active-image"
 
 export default function Editor () {
     return (
         <div className="flex h-full ">
-            <div className="py-6 px-4 basis[360px] shrink-0">
+            <div className="py-6 px-4 basis-[220px] shrink-0">
                 <div className="pb-12 text-center ">
                     <ModeToggle />
                 </div>
             </div>
             <UploadImage />
+            <ActiveImage />
             <Layers />
         </div>
     )
