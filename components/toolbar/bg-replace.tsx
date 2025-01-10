eb195ec3c279ea74bf117671ca024c2272e4e5de
dimitri <dyaryarashvili@gmail.com>
2025-01-11 02:17:42 +0400

    feat: added background replace function

--- components/toolbar/bg-replace.tsx
+++ components/toolbar/bg-replace.tsx
@@ -4,33 +4,44 @@ import { useLayerStore } from "@/lib/layer-store"
 import { useImageStore } from "@/lib/image-store"
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { Button } from "../ui/button"
-import { Image } from "lucide-react"
+import { Image, ImageOff } from "lucide-react"
 import { bgRemove } from "@/server/bg-remove"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import { useState } from "react"
+import { bgReplace } from "@/server/bg-replace"
 
 
-export default function BgRemove() {
+export default function BgReplace() {
     const setGenerating = useImageStore((state) => state.setGenerating)
     const generating = useImageStore((state) => state.generating)
     const activeLayer = useLayerStore((state) => state.activeLayer)
     const addLayer = useLayerStore((state) => state.addLayer)
     const setActiveLayer = useLayerStore((state) => state.setActiveLayer)
 
+    const [prompt, setPrompt] = useState("")
+
     return (
         <Popover>
             <PopoverTrigger disabled={!activeLayer?.url} asChild>
                 <Button variant="outline" className="p-8">
                     <span className="flex gap-1 items-center justify-center flex-col text-xs font-medium">
-                        Background removal <Image size={20} />
+                        Background replace <ImageOff size={20} />
                     </span>
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-full">
                 <div>
-                    <h3>Background removal</h3>
+                    <h3>Background replace</h3>
                     <p className="text-sm text-muted-foreground">
-                        Remove the background of an image with one click
+                        Replace the background of an image with one click
                     </p>
                 </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="prompt" >Prompt</Label>
+                    <Input id="prompt" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Describe new background" />
+                </div>
+
                 <Button 
                 disabled={!activeLayer?.url || generating}
                 className="w-full mt-4"
@@ -38,19 +49,19 @@ export default function BgRemove() {
                     const newLayerId = crypto.randomUUID();
 
                     setGenerating(true);
-                    const res = await bgRemove({
+                    const res = await bgReplace({
                         activeImage: activeLayer.url!,
-                        format: activeLayer.format!,
+                        prompt: prompt,
                     })
 
                     if(res?.data?.success){
                         addLayer({
                             id: newLayerId,
                             url: res.data.success,
-                            format: "png",
+                            format: activeLayer.format,
                             height: activeLayer.height,
                             width: activeLayer.width,
-                            name: "BgRemoved" + activeLayer.name,
+                            name: "BgReplaced" + activeLayer.name,
                             publicId: activeLayer.publicId,
                             resourceType: "image",
                         })
@@ -63,7 +74,7 @@ export default function BgRemove() {
                     }
                 }}
                 >
-                    {generating ? 'Removing...' : 'Remove background'}
+                    {generating ? 'Replacing...' : 'Replace background'}
                 </Button>
             </PopoverContent>
         </Popover>
